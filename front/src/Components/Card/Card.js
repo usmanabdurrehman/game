309ed41e92a5b03d0dbf56e5e13ad713f6e49b33
usmanabdurrehman <saidskit@outlook.com>
2021-03-01 11:56:31 +0500

    Added gradients for pricing cards

--- front/src/Components/Card/Card.js
+++ front/src/Components/Card/Card.js
@@ -1,10 +1,21 @@
-import React from 'react'
-import styles from './Card.module.css'
+import React from "react";
+import styles from "./Card.module.css";
 
-export default function Card({card:{price}}) {
+export default function Card({ card: { price,id,services } }) {
+	let linGrad = [
+		"linear-gradient(to bottom, #0575E6, #021B79)",
+		"linear-gradient(to bottom, #8e0e00, #1f1c18)",
+		"linear-gradient(to bottom, #f00000, #dc281e)",
+	];
+
 	return (
-		<div className={styles.card}>
-			<div><h1>{`${price} $`}</h1></div>
+		<div className={styles.card} style={{backgroundImage:linGrad[id-1]}}>
+			<div className={styles.cardPrice}>
+				<h1>{`${price}$`}</h1>
+			</div>
+			<div className={styles.cardServices}>
+				{services.map(service=><div>{service}</div>)}
+			</div>
 		</div>
-	)
+	);
 }
--- front/src/Pages/Pricing/Pricing.js
+++ front/src/Pages/Pricing/Pricing.js
@@ -6,13 +6,16 @@ export default function Pricing() {
 
 	let data = [{
 		id:1,
-		price:39.99
+		price:39.99,
+		services:['1 month free trial','100% money back guarantee']
 	},{
 		id:2,
-		price:59.99
+		price:59.99,
+		services:['1 month free trial','100% money back guarantee','5 Copies of the game']
 	},{
 		id:3,
-		price:99.99
+		price:99.99,
+		services:['1 month free trial','100% money back guarantee','10 Copies of the game']
 	}]
 
 	return (
